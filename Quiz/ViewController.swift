cadef0f24354dd80720703120454ae4d9f236c9e
Zeeshan <zeeshan.12.zw@gmail.com>
2024-06-04 20:05:04 +0500

    set bg images and coors

---
+++ Quiz/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Zeeshan Waheed on 04/06/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var imageBG: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        imageBG.layer.cornerRadius = 20
+    }
+
+
+}
+
